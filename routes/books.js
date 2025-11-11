c363f42b6f8bf4cd549ac29dbe240295122e38ad
E H Murdoch <ehmurdoch47@protonmail.com>
2025-11-11 10:27:49 +0000

    Initial commit.

--- routes/books.js
+++ routes/books.js
@@ -1,15 +1,26 @@
 // Create a new router
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-router.get('/search',function(req, res, next){
-    res.render("search.ejs")
+router.get('/search', function(req, res, next) {
+    res.render("search.ejs");
 });
 
 router.get('/search-result', function (req, res, next) {
     //searching in the database
-    res.send("You searched for: " + req.query.keyword)
+    res.send("You searched for: " + req.query.keyword);
+});
+
+router.get('/list', function(req, res, next) {
+    // query database to get all the books
+    let sqlquery = "SELECT * FROM books";
+    // execute sql query
+    db.query(sqlquery, (err, result) => {
+        if (err)
+            next(err);
+        res.send(result);
+    });
 });
 
 // Export the router object so index.js can access it
-module.exports = router
+module.exports = router;
