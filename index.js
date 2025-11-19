c02779b76f4254444e4db9fdfdbd579efc5b7ba3
E H Murdoch <ehmurdoch47@protonmail.com>
2025-11-19 15:35:25 +0000

    Change environment variable names to fit marking criteria.

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ dotenv.config();
 
 // Define the database connection pool
 const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    host: process.env.BB_HOST,
+    user: process.env.BB_USER,
+    password: process.env.BB_PASSWORD,
+    database: process.env.BB_DATABASE,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
